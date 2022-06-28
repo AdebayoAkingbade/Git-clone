e7562b3a4f1da09ee471d747114829b465332c2a
adebayoakingbade <adebayo.akingbade@decagon.dev>
2022-06-28 13:25:04 +0100

    darkmode in process

--- src/App.js
+++ src/App.js
@@ -1,11 +1,24 @@
+import { createContext, useState } from 'react';
 import './App.css';
 import User from './utils/User';
+import ReactSwitch from 'react-switch'
+
+export const ThemeContext = createContext(null)
 
 function App() {
+  const [ theme, setTheme] = useState('light')
+
+  const changeTheme = ()=>{
+    setTheme((curr)=> (curr === 'light' ? 'dark' : 'light'))
+  }
   return (
-    <div className="App">
-      <User />
-    </div>
+    <ThemeContext.Provider value={{ theme, changeTheme}}>
+      <div className="App" id={theme}>
+        <User />
+        <label>{theme === 'light' ? 'Light Mode' : 'Dark Mode'}</label>
+      <ReactSwitch onChange={changeTheme} checked={theme === 'dark'}/>
+      </div>
+    </ThemeContext.Provider>
   );
 }
 
--- src/utils/User.js
+++ src/utils/User.js
@@ -3,9 +3,8 @@ import Header from '../components/navbar/Navbar'
 import Tab from '../components/tabs/Tab'
 import UserInfo from '../components/userInfo/UserInfo'
 import { Container, Grid } from '@material-ui/core'
-import { BrowserRouter as Router, Routes, Route, } from 'react-router-dom'
+import { BrowserRouter as Router, Routes, } from 'react-router-dom'
 import Repos from '../components/repositories/Repos'
-import DarkMode from '../components/darkMode/DarkMode'
 
 function User() {
   return (
@@ -13,7 +12,6 @@ function User() {
       <Router>
       <Fragment>
         <Header/>
-        <DarkMode />
         <Container>
           <Grid container spacing={10}>
             <Grid item xs={12} sm={12} md={4} lg={3} style={{ boxShadow: '0px, 2px, 92px, 0px rgba(0, 0, 0, 0.13)' }}>
